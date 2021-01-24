aa7ed082ad91f3e0fe8f81e2a30838f9232f412f
Egor <ze99@rambler.ru>
2021-01-24 14:00:06 +0300

    add lab3

---
+++ jflap/lab3/Email/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Email
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("email.txt", System.Text.Encoding.UTF8);
+            string text = sr.ReadToEnd();
+
+            Regex r = new Regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+.[a-zA-Z]{2,4}"); 
+            MatchCollection me = r.Matches(text);
+
+            for (int i = 0; i < me.Count; i++)
+                Console.WriteLine(me[i].Value);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ jflap/lab3/FA-Model/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace FA_Model
+{
+    class Program
+    {
+        static bool InMainRange(char ch)
+        {
+            return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ||
+                (ch >= '0' && ch <= '9') || (ch == '_' || ch == '_') ;
+        }
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine() + '\0';
+            char state = '1';
+            int i = 0;
+            char symbol;
+            try
+            {
+                while ((symbol = s[i++]) != '\0')
+                {
+                    switch (state)
+                    {
+                        case '1':
+                            if (InMainRange(symbol) || symbol == '.')
+                            {
+                                state = '2';
+                            }
+                            else
+                            {
+                                throw new Exception();
+
+                            }
+                            break;
+                        case '2':
+                            if (InMainRange(symbol) || symbol == '.')
+                            {
+                                state = '2';
+                            }
+                            else if (symbol == '@')
+                            {
+                                state = '3';
+                            }
+                            else
+                            {
+                                throw new Exception();
+
+                            }
+                            break;
+                        case '3':
+                            if (InMainRange(symbol))
+                            {
+                                state = '4';
+                            }
+                            else
+                            {
+                                throw new Exception();
+
+                            }
+                            break;
+                        case '4':
+                            if (InMainRange(symbol))
+                            {
+                                state = '4';
+                            }
+                            else if (symbol == '.')
+                            {
+                                state = '5';
+                            }
+                            else
+                            {
+                                throw new Exception();
+                            }
+                            break;
+                        case '5':
+                            if (InMainRange(symbol))
+                            {
+                                state = 'F';
+                            }
+                            else if (symbol == '.')
+                            {
+                                state = '5';
+                            }
+                            else
+                            {
+                                throw new Exception();
+                            }
+                            break;
+
+                    }
+                }
+                Console.WriteLine(state == 'F');
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("обнаружен недопустимый символ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
