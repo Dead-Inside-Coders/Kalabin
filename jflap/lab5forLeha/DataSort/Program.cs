ad59de8cdf06222ca852d2dcfa056909991fc841
Egor <ze99@rambler.ru>
2021-01-31 15:31:36 +0300

    add all labs

---
+++ jflap/lab5forLeha/DataSort/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class TuringMachine
+{
+    public static void Main()
+    {
+
+        var sorter = new TuringMachine("A", '*', "X").WithTransitions(
+            ("A", 'a', 'a', Right, "A"),
+            ("A", 'b', 'B', Right, "B"),
+            ("A", '*', '*', Left, "E"),
+            ("B", 'a', 'a', Right, "B"),
+            ("B", 'b', 'b', Right, "B"),
+            ("B", '*', '*', Left, "C"),
+            ("C", 'a', 'b', Left, "D"),
+            ("C", 'b', 'b', Left, "C"),
+            ("C", 'B', 'b', Left, "E"),
+            ("D", 'a', 'a', Left, "D"),
+            ("D", 'b', 'b', Left, "D"),
+            ("D", 'B', 'a', Right, "A"),
+            ("E", 'a', 'a', Left, "E"),
+            ("E", '*', '*', Right, "X")
+        )
+        .WithInput("babbababaa");
+
+        Console.WriteLine("babbababaa");
+        sorter.Run().Last();
+        Console.WriteLine("Sorted: " + sorter.TapeString);
+        PrintResults(sorter);
+
+
+        Console.WriteLine("bbbababaaabba");
+        sorter.Reset().WithInput("bbbababaaabba");
+        sorter.Run().Last();
+        Console.WriteLine("Sorted: " + sorter.TapeString);
+        PrintResults(sorter);
+
+        void PrintResults(TuringMachine tm)
+        {
+            Console.WriteLine($"End state: {tm.State} = {(tm.Success ? "Success" : "Failure")}");
+            Console.WriteLine(tm.Steps + " steps");
+            Console.WriteLine("tape length: " + tm.TapeLength);
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+
+    public const int Left = -1, Stay = 0, Right = 1;
+    private readonly Tape tape;
+    private readonly string initialState;
+    private readonly HashSet<string> terminatingStates;
+    private Dictionary<(string state, char read), (char write, int move, string toState)> transitions;
+
+    public TuringMachine(string initialState, char blankSymbol, params string[] terminatingStates)
+    {
+        State = this.initialState = initialState;
+        tape = new Tape(blankSymbol);
+        this.terminatingStates = terminatingStates.ToHashSet();
+    }
+
+    public TuringMachine WithTransitions(
+        params (string state, char read, char write, int move, string toState)[] transitions)
+    {
+        this.transitions = transitions.ToDictionary(k => (k.state, k.read), k => (k.write, k.move, k.toState));
+        return this;
+    }
+
+    public TuringMachine Reset()
+    {
+        State = initialState;
+        Steps = 0;
+        tape.Reset();
+        return this;
+    }
+
+    public TuringMachine WithInput(string input)
+    {
+        tape.Input(input);
+        return this;
+    }
+
+    public int Steps { get; private set; }
+    public string State { get; private set; }
+    public bool Success => terminatingStates.Contains(State);
+    public int TapeLength => tape.Length;
+    public string TapeString => tape.ToString();
+
+    public IEnumerable<string> Run()
+    {
+        yield return State;
+        while (Step()) yield return State;
+    }
+
+    public async Task<TimeSpan> TimeAsync(CancellationToken cancel = default)
+    {
+        var chrono = Stopwatch.StartNew();
+        await RunAsync(cancel);
+        chrono.Stop();
+        return chrono.Elapsed;
+    }
+
+    public Task RunAsync(CancellationToken cancel = default)
+        => Task.Run(() => {
+            while (Step()) cancel.ThrowIfCancellationRequested();
+        });
+
+    private bool Step()
+    {
+        if (!transitions.TryGetValue((State, tape.Current), out var action)) return false;
+        tape.Current = action.write;
+        tape.Move(action.move);
+        State = action.toState;
+        Steps++;
+        return true;
+    }
+
+
+    private class Tape
+    {
+        private List<char> forwardTape = new List<char>(), backwardTape = new List<char>();
+        private int head = 0;
+        private char blank;
+
+        public Tape(char blankSymbol) => forwardTape.Add(blank = blankSymbol);
+
+        public void Reset()
+        {
+            backwardTape.Clear();
+            forwardTape.Clear();
+            head = 0;
+            forwardTape.Add(blank);
+        }
+
+        public void Input(string input)
+        {
+            Reset();
+            forwardTape.Clear();
+            forwardTape.AddRange(input);
+        }
+
+        public void Move(int direction)
+        {
+            head += direction;
+            if (head >= 0 && forwardTape.Count <= head) forwardTape.Add(blank);
+            if (head < 0 && backwardTape.Count <= ~head) backwardTape.Add(blank);
+        }
+
+        public char Current
+        {
+            get => head < 0 ? backwardTape[~head] : forwardTape[head];
+            set
+            {
+                if (head < 0) backwardTape[~head] = value;
+                else forwardTape[head] = value;
+            }
+        }
+
+        public int Length => backwardTape.Count + forwardTape.Count;
+
+        public override string ToString()
+        {
+            int h = (head < 0 ? ~head : backwardTape.Count + head) * 2 + 1;
+            var builder = new StringBuilder(" ", Length * 2 + 1);
+            if (backwardTape.Count > 0)
+            {
+                builder.Append(string.Join(" ", backwardTape)).Append(" ");
+                if (head < 0) (builder[h + 1], builder[h - 1]) = ('(', ')');
+                for (int l = 0, r = builder.Length - 1; l < r; l++, r--) (builder[l], builder[r]) = (builder[r], builder[l]);
+            }
+            builder.Append(string.Join(" ", forwardTape)).Append(" ");
+            if (head >= 0) (builder[h - 1], builder[h + 1]) = ('(', ')');
+            return builder.ToString();
+        }
+
+    }
+
+}
---
+++ jflap/lab7/turing/turing/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+
+namespace turing
+{
+    class TuringMachine
+    {
+        [DllImport ("user32.dll", EntryPoint = "GetKeyState")]
+        public static extern short GetKeyState(int nVirtKey);
+        struct LeftSide
+        {
+            public int state;
+            public char symbol;
+
+            public LeftSide(int st,char sym)
+            {
+                state = st;
+                symbol = sym;
+
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is LeftSide side &&
+                       state == side.state &&
+                       symbol == side.symbol;
+            }
+
+            public override int GetHashCode()
+            {
+                int hashCode = 58075447;
+                hashCode = hashCode * -1521134295 + state.GetHashCode();
+                hashCode = hashCode * -1521134295 + symbol.GetHashCode();
+                return hashCode;
+            }
+        }
+        struct RightSide
+        {
+            public int state;
+            public char symbol, command;
+
+            public RightSide(int st,char sym, char c)
+            {
+                state = st;
+                symbol = sym;
+                command = c;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is RightSide side &&
+                       state == side.state &&
+                       symbol == side.symbol &&
+                       command == side.command;
+            }
+
+            public override int GetHashCode()
+            {
+                int hashCode = 2134386561;
+                hashCode = hashCode * -1521134295 + state.GetHashCode();
+                hashCode = hashCode * -1521134295 + symbol.GetHashCode();
+                hashCode = hashCode * -1521134295 + command.GetHashCode();
+                return hashCode;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int Sstate = Convert.ToInt32(Console.In.ReadLine());
+            int Ystate = Convert.ToInt32(Console.In.ReadLine());
+            int Nstate = Convert.ToInt32(Console.In.ReadLine());
+
+            string Tape = ">" + (string)Console.In.ReadLine() + "_";
+
+            Hashtable rules = new Hashtable();
+            string s;
+
+            while ((s = Console.In.ReadLine()) != "")
+            {
+                string[] tr = s.Split(' ');
+
+                rules.Add(new LeftSide(Convert.ToInt32(tr[0]), Convert.ToChar(tr[1])),
+                    new RightSide(Convert.ToInt32(tr[2]), Convert.ToChar(tr[3]), Convert.ToChar(tr[4])));
+
+            }
+            int State = Sstate;
+            int TapeIdx = 1;
+            const int VK_SPACE = 0x20;
+            try
+            {
+                while (State != Ystate && State != Nstate)
+                {
+                    if (((int)GetKeyState(VK_SPACE) & 0x8000) != 0)
+                    {
+                        Console.WriteLine("Работа прервана");
+                        return;
+                    }
+
+                    RightSide r = (RightSide)rules[new LeftSide(State, Tape[TapeIdx])];
+                    State = r.state;
+                    Tape = (Tape.Remove(TapeIdx, 1)).Insert(TapeIdx, r.symbol.ToString());
+                    if (r.command == 'L')
+                    {
+                        TapeIdx--;
+                    }
+                    else if (r.command == 'R')
+                    {
+                        TapeIdx++;
+                    }
+                    if (TapeIdx >= Tape.Length)
+                    {
+                        Tape += "_";
+                    }
+                }
+                Console.WriteLine(State == Ystate ? "Строка допущена" : "Строка отвергнута");
+            } catch (Exception)
+            {
+                Console.WriteLine("Строка отвергнута");
+            }
+
+            Console.WriteLine("Лента: " + Tape);
+
+            Console.ReadKey();
+
+        }
+    }
+}
