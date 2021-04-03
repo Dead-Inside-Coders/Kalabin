28e51be6661ca2bbeb6a80d5e8d3dbf7bd28be77
Egor <ze99@rambler.ru>
2021-04-03 12:48:14 +0300

    add tsp kurs work

---
+++ Коммивояжер/TravelingSalesman/src/ru/tver/tstu/tsp/approximate/Edge.java
@@ -0,0 +1,10 @@
+package ru.tver.tstu.tsp.approximate;
+
+public class Edge {
+    public int v, w; Double coste;
+    Edge(int v1, int v2, Double c) {
+        v = v1;
+        w = v2;
+        coste = c;
+    }
+}
